ac458b1145f4d820a6cfa93e606c4587afba2ffa
otcz <54145021+otcz@users.noreply.github.com>
2021-11-03 22:29:06 -0500

    USER

--- src/municione/inicio/control/ValidarUsuario.java
+++ src/municione/inicio/control/ValidarUsuario.java
@@ -1,14 +1,23 @@
 package municione.inicio.control;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ValidarUsuario implements ActionListener {
 
-    JButton button;
+    String usuario, clave;
+    DatosUsuarioBD datosUsuarioBD = new DatosUsuarioBD();
+
+    public ValidarUsuario(String usuario, String clave) {
+        this.usuario = usuario;
+        this.clave = clave;
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (usuario.equals(datosUsuarioBD.getUsurio()) && clave.equals(datosUsuarioBD.getClave())) {
+            
+        }
     }
 }
